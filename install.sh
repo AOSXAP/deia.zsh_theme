ecacf29ed283db0d2cadeb61c1ad573360789749
AOSXAP <74103250+AOSXAP@users.noreply.github.com>
2022-12-04 17:32:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo rm -rf ./deia.zsh_theme
 
 #clone repo and copy into oh-my-zsh folder
-cp ./deia.zsh-theme $USER/.oh-my-zsh/themes/
+cp ./deia.zsh-theme /home/$USER/.oh-my-zsh/themes/
 
 #reconfig zshrc file
-sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="deia"/' $USER/.zshrc
+sed -i 's/.*ZSH_THEME=.*/ZSH_THEME="deia"/' /home/$USER/.zshrc
